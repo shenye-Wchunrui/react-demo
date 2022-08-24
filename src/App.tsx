b5cd5329d88eacb4eea6ccb6ab051228412a26f2
wenchunrui <w_chunrui@163.com>
2022-08-24 22:32:48 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+/*
+ * @Author: wenchunrui w_chunrui@163.com
+ * @Date: 2022-08-24 22:06:11
+ * @LastEditors: wenchunrui w_chunrui@163.com
+ * @LastEditTime: 2022-08-24 22:08:42
+ * @FilePath: \my-react-ts\src\App.tsx
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by wenchunrui w_chunrui@163.com, All Rights Reserved. 
+ */
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App(a) {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
